a7eb7f11f8c9497c7a6c13818e610d09c089b99b
lingkongc-work <792764556@qq.com>
2019-11-11 12:15:27 +0800

    :construction: add dependence and modifiy some files

--- src/util/ling.js
+++ src/util/ling.js
@@ -1,32 +1,36 @@
 import $ from 'jquery';
 
+// eslint-disable-next-line no-underscore-dangle
 const _ling = {
-    // 发送请求
-    request: function (param) {
-        $.ajax({
-            type: param.method || 'GET',
-            url: param.url || '',
-            dataType: param.type || 'json',
-            data: param.data || '',
-            success: function (res) {
-                if (200 === res.status) {
-                    typeof param.success === 'function' && param.success(res.data, res.msg);
-                } else if (4000 === res.status()) {
-                    //    未登录 需要强制登录
+  // 发送请求
+  request(param) {
+    $.ajax({
+      type: param.method || 'GET',
+      url: param.url || '',
+      dataType: param.type || 'json',
+      data: param.data || '',
+      success(res) {
+        if (res.status === 200) {
+          // eslint-disable-next-line no-unused-expressions
+          typeof param.success === 'function' && param.success(res.data, res.msg);
+        } else if (res.status() === 4000) {
+          //    未登录 需要强制登录
 
-                }
-            },
-            error: function (err) {
-                typeof param.error === 'function' && param.error(err.msg);
-            }
-        });
-    },
-    // 获取URL参数
-    getUrlParam: function (name) {
-        const reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
-        const result = window.location.search.substr(1).match(reg);
-        return result ? decodeURIComponent(result[2]) : null;
-    }
+        }
+      },
+      error(err) {
+        // eslint-disable-next-line no-unused-expressions
+        typeof param.error === 'function' && param.error(err.msg);
+      },
+    });
+  },
+  // 获取URL参数
+  getUrlParam(name) {
+    const reg = new RegExp(`(^|&)${name}=([^&]*)(&|$)`);
+    const result = window.location.search.substr(1)
+      .match(reg);
+    return result ? decodeURIComponent(result[2]) : null;
+  },
 };
 
 export default _ling;
